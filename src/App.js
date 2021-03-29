a26aef15bead196f83bd77a26ce3973bccd60976
johnbigornia <jbigornia15@gmail.com>
2021-03-28 20:45:35 -1000

    Finished a basic landing page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>YO</h1>
-    </div>
-  );
+import { Project } from './pages/Project';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { NoMatch } from './pages/NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+ 
+        <NavigationBar />
+        <div 
+          style={{backgroundColor: 'black', transparent: false}}
+        /> 
+        <Layout>
+        <div 
+          style={{backgroundColor: 'black', transparent: false}}
+        /> 
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/project" component={Project} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
